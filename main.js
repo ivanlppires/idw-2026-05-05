0087486da1062bf90d57e03315e2ea5109c289d0
Ivan Pires <ivanpires@gmail.com>
2026-05-05 20:07:08 -0400

    exemplo com switch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* ESTRUTURAS DE CONTROLE E REPETIÇÃO */
+/* ESTRUTURAS DE CONTROLE */
 
 // Estrutura de controle: if, else if, else
 
@@ -26,3 +26,43 @@ let aprovado = (nota1+nota2+nota3)/3 >= 6 ? "Aprovado" : "Reprovado";
 
 console.log(maioridade);
 console.log(aprovado);
+
+/* **** Estrutura de controle: switch case ***/
+
+let dia = 3;
+
+switch (dia) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-feira");
+        break;
+    case 4:
+        console.log("Quarta-feira");
+        break;
+    case 5:
+        console.log("Quinta-feira");
+        break;
+    case 6:
+        console.log("Sexta-feira");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Dia inválido.");
+}
+
+// É possível fazer a mesma coisa com if else if, mas o switch case é mais organizado e fácil de ler quando temos muitas condições para verificar.
+if (dia == 1) console.log("Domingo");
+else if (dia == 2) console.log("Segunda-feira");
+else if (dia == 3) console.log("Terça-feira");
+else if (dia == 4) console.log("Quarta-feira");
+else if (dia == 5) console.log("Quinta-feira");
+else if (dia == 6) console.log("Sexta-feira");
+else if (dia == 7) console.log("Sábado");
+else console.log("Dia inválido.");
