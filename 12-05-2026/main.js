ae3eea4205c9dc7f40170fb4f6a50277ac5c450e
Ivan Pires <ivanpires@gmail.com>
2026-05-12 20:28:36 -0400

    exercicio calculadora resolvido

--- 12-05-2026/main.js
+++ 12-05-2026/main.js
@@ -1,43 +1,25 @@
-/* FUNÇÕES EM JAVACRIPT */
+/* EXERCÍCIO CALCULADORA EM JS */
 
-// Função - Tradicional
-function soma(a, b) {
-    return a + b;
-}
+const soma = (a, b) => a + b;
+const subtracao = (a, b) => a - b;
+const multiplicacao = (a, b) => a * b;
+const divisao = (a, b) => (b>0) ? a / b : "Divisão por zero não é permitida";
 
-// Função Anônima - atribuída a uma variável
-let multiplicacao = function(a, b) {
-    return a * b;
-}
-
-/* ### Arrow Function - Forma mais moderna de escrever funções ### */
-
-// Arrow Function com vários parâmetros e várias expressões
-const divisao = (a, b) => {
-    if (b === 0) return "Divisão por zero não é permitida.";
-    return a / b;
-}
-
-// Arrow Function com 1 parâmetro e várias expressões. Parênteses são opcionais.
-const ehPar = numero => {
-    const par = (numero % 2 === 0) ? true : false;
-    if(par) return "O número é par.";
-    return "O número é ímpar.";
-}
-
-// Arrow Function com 1 parâmetro e 1 expressão. Parênteses, chaves e return são opcionais.
-const quadrado = numero => numero * numero;
-
-// Arrow Function sem parâmetros
-const numeroSorte = () => "Seu número da sorte é " + Math.ceil(Math.random() * 10);
-
-console.log(soma(2,3))
-console.log(divisao(10,0))
-console.log(multiplicacao(2,8))
-console.log(ehPar(7))
-console.log(quadrado(5))
-console.log(numeroSorte())
-
-// exercicio: faça uma calculadora. Use o prompt para receber número. Exemplo:
 const num1 = parseInt(prompt("Digite o primeiro número:"));
-console.log("Voê digitou o número " + num1);
+const num2 = parseInt(prompt("Digite o segundo número:"));
+const operacao = prompt("Digite a operação (+, -, *, /):");
+
+let resultado;
+
+if(operacao === "+") resultado = soma(num1, num2);
+else if(operacao === "-") resultado = subtracao(num1, num2);
+else if(operacao === "*") resultado = multiplicacao(num1, num2);
+else if(operacao === "/") resultado = divisao(num1, num2);
+else resultado = "Operação inválida.";
+
+console.log("Resultado: " + resultado);
+
+/* OBSERVAÇÃO */
+// = é uma atribuição
+// == é uma comparação de valor (pode ser perigoso, pois 0 == false é true)
+// === é uma comparação de valor e tipo (recomendada, pois 0 === false é false)
